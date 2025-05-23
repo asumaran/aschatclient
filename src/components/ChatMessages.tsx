21aadd1433b29335919bcbb6459f7cb02eebf037
Alfredo Sumaran <a.sumaran@gmail.com>
2025-05-23 00:48:40 -0500

    Load messages for the active channel

--- src/components/ChatMessages.tsx
+++ src/components/ChatMessages.tsx
@@ -1,23 +1,27 @@
 import { useEffect, useState } from 'react';
 import ChatMessage from './ChatMessage';
+import { useChatContext } from '@/useChatContext';
 
 const ChatMessages = () => {
+  const { activeChannelId } = useChatContext();
+
   const [messages, setMessages] = useState<{ content: string }[]>([]);
 
   useEffect(() => {
     async function getChannelMessages() {
-      const channelId = 1;
       const response = await fetch(
-        `http://localhost:4000/messages/${channelId}`,
+        `http://localhost:4000/messages/${activeChannelId}`,
       );
       const msgs = await response.json();
       setMessages(msgs);
     }
 
     getChannelMessages();
-  }, []);
+  }, [activeChannelId]);
 
-  return messages.map((m) => <ChatMessage content={m.content} />);
+  return messages.length
+    ? messages.map((m) => <ChatMessage content={m.content} />)
+    : 'No messages';
 };
 
 export default ChatMessages;
