995ea84012ac04f3dcda621cc0a3b23ed9925a44
Alfredo Sumaran <a.sumaran@gmail.com>
2025-05-28 19:29:12 -0500

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import ChannelList from './components/ChannelList';
 import ChatMainPanel from './components/ChatMainPanel';
+import { useState } from 'react';
+import SignInForm from './components/SignInForm';
 
 const queryClient = new QueryClient();
 
 function App() {
+  const [showLoginForm] = useState(true);
+
   return (
     <QueryClientProvider client={queryClient}>
       <div className="@container/main p-10">
         <div className="m-auto flex w-7xl">
-          <div className="m-5 mr-0 border-1">
-            <ChannelList />
-          </div>
-          <div className="m-5 flex-1 border-1">
-            <ChatMainPanel />
-          </div>
+          {showLoginForm ? (
+            <SignInForm />
+          ) : (
+            <>
+              <div className="m-5 mr-0 border-1">
+                <ChannelList />
+              </div>
+              <div className="m-5 flex-1 border-1">
+                <ChatMainPanel />
+              </div>
+            </>
+          )}
         </div>
       </div>
     </QueryClientProvider>
---
+++ src/components/SignInForm.tsx
@@ -0,0 +1,74 @@
+import { useForm } from 'react-hook-form';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from './ui/form';
+import { Input } from './ui/input';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button } from './ui/button';
+
+const formSchema = z.object({
+  email: z.string().min(2, {
+    message: 'Email must be at least 2 characters.',
+  }),
+  password: z.string().min(4, {
+    message: 'Password must be at least 4 characters.',
+  }),
+});
+
+const SignInForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log({ values });
+  }
+
+  return (
+    <div>
+      <Form {...form}>
+        <form className="space-y-5" onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default SignInForm;
