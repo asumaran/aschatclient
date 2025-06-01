ee66355bed278323b5414a63677910d779a17465
Alfredo Sumaran <a.sumaran@gmail.com>
2025-06-01 16:25:37 -0500

    Show sign-out link only if the user is authenticated

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,24 +1,35 @@
+import { useChatContext } from '@/useChatContext';
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 
 export const Route = createRootRoute({
-  component: () => (
+  component: RootComponent,
+});
+
+function RootComponent() {
+  const { activeUserId } = useChatContext();
+
+  return (
     <>
-      <div className="flex gap-2 p-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{' '}
-        <Link to="/chat" className="[&.active]:font-bold">
-          Chat
-        </Link>{' '}
-        <Link to="/about" className="[&.active]:font-bold">
-          About
-        </Link>
-        <Link to="/signout">Sign out</Link>
+      <div className="flex">
+        <div className="flex-1 p-2">
+          <Link to="/" className="[&.active]:font-bold">
+            Home
+          </Link>{' '}
+          <Link to="/chat" className="[&.active]:font-bold">
+            Chat
+          </Link>{' '}
+          <Link to="/about" className="[&.active]:font-bold">
+            About
+          </Link>
+        </div>
+        <div className="p-2">
+          {activeUserId && <Link to="/signout">Sign out</Link>}
+        </div>
       </div>
       <hr />
       <Outlet />
       <TanStackRouterDevtools />
     </>
-  ),
-});
+  );
+}
